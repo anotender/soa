f2c61baee66f6144e2f484079c818679dd457ab3
Mateusz Nowak <mateusz.nowak@comarch.pl>
2017-05-23 12:40:30 +0200

    Added persistence.xml and entity manager

--- lab8/src/main/java/pl/edu/agh/soa/lab8/zad3/repository/BookRepository.java
+++ lab8/src/main/java/pl/edu/agh/soa/lab8/zad3/repository/BookRepository.java
@@ -1,89 +1,43 @@
 package pl.edu.agh.soa.lab8.zad3.repository;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import pl.edu.agh.soa.lab8.zad3.model.Book;
-import pl.edu.agh.soa.lab8.zad3.util.HibernateUtil;
 
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Singleton
 @Startup
 public class BookRepository {
 
-    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    @PersistenceContext(name = "books-pu")
+    private EntityManager em;
 
     public List<Book> findAll() {
-        Session session = getSessionAndBeginTransaction();
-
-        List<Book> books = (List<Book>) session
+        return (List<Book>) em
                 .createQuery("from pl.edu.agh.soa.lab8.zad3.model.Book")
-                .list();
-
-        commitTransactionAndCloseSession(session);
-        return books;
+                .getResultList();
     }
 
     public Optional<Book> findOne(long id) {
-        Session session = getSessionAndBeginTransaction();
-
-        Object result = session
-                .createQuery("" +
-                        "from pl.edu.agh.soa.lab8.zad3.model.Book as b " +
-                        "where b.id = " + id
-                )
-                .uniqueResult();
-
-        commitTransactionAndCloseSession(session);
-
-        if (Objects.isNull(result)) {
-            return Optional.empty();
-        }
-        return Optional.of((Book) result);
+        return Optional.ofNullable(em.find(Book.class, id));
     }
 
     public void save(Book b) {
-        Session session = getSessionAndBeginTransaction();
-
-        session.save(b);
-
-        commitTransactionAndCloseSession(session);
+        em.persist(b);
     }
 
     public void update(long id, Book newBook) {
         findOne(id).ifPresent(oldBook -> {
-            Session session = getSessionAndBeginTransaction();
-
             newBook.setId(oldBook.getId());
-            session.update(newBook);
-
-            commitTransactionAndCloseSession(session);
-
+            em.merge(newBook);
         });
     }
 
     public void delete(long id) {
-        findOne(id).ifPresent(b -> {
-            Session session = getSessionAndBeginTransaction();
-
-            session.delete(b);
-
-            commitTransactionAndCloseSession(session);
-        });
-    }
-
-    private Session getSessionAndBeginTransaction() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        return session;
-    }
-
-    private void commitTransactionAndCloseSession(Session session) {
-        session.getTransaction().commit();
-        session.close();
+        findOne(id).ifPresent(em::remove);
     }
 }
